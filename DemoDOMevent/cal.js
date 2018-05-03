55c760a6f626f5f8dbf9ab7842b6c16cb177d218
ThanhTam_90HP <thanhtam.svhpu121161@gmail.com>
2018-05-03 13:11:30 +0700

    update

---
+++ DemoDOMevent/cal.js
@@ -0,0 +1,28 @@
+function btnAdd() {
+    document.Calculator.display.value += "+";
+    document.Calculator.display.style.textAlign = "right";
+}
+function btnSub() {
+    document.Calculator.display.value += "-";
+    document.Calculator.display.style.textAlign = "right";
+}
+function btnMult() {
+    document.Calculator.display.value += "*";
+    document.Calculator.display.style.textAlign = "right";
+}
+function btnDiv() {
+    document.Calculator.display.value += "/";
+    document.Calculator.display.style.textAlign = "right";
+}
+function btnMod() {
+    document.Calculator.display.value += "%";
+    document.Calculator.display.style.textAlign = "right";
+}
+function btnDot() {
+    document.Calculator.display.value += ".";
+    document.Calculator.display.style.textAlign = "right";
+}
+function btnClear() {
+    document.Calculator.display.value += "";
+    document.Calculator.display.style.textAlign = "right";
+}
