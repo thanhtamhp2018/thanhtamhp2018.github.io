b99ba42e20ca040822792622b70ae87cca057ef3
Lê Thanh Tâm <37645432+thanhtamhp2018@users.noreply.github.com>
2018-05-19 20:09:39 +0700

    update

---
+++ Learn Bootstrap/main.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+var app = new Vue({
+	el:'#app',//đây là id của element cần đổ dữ liệu vào
+	data:{
+		product: 'Đồ lót sexy', //dữ liệu để rót vào element
+		image:'image/1.jpg',
+		url:'https://photos-5.dropbox.com/t/2/AADT4FR1OB8L03zWYwbGTTPdhCBo-me6UrYbY2292dXdfw/12/703122049/jpeg/32x32/3/1526745600/0/2/vmSocks-green-onWhite.jpg/EJDIgOMGGNAIIAcoBw/8upE_sHYFbWfmd6CjMSrsyskgSg84nU-tJM2U0wZK8g?dl=0&size=1600x1200&size_mode=3'
+	}
+})
---
+++ js learning/test.js
@@ -0,0 +1,4 @@
+function message() {
+	let a="chervolet camaro SS";
+	document.writeln(a);
+}
