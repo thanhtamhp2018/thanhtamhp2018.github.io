99c88ca22cbe888f9b22dd6677697b80cc97f3c7
Lê Thanh Tâm <37645432+thanhtamhp2018@users.noreply.github.com>
2018-05-22 14:28:46 +0700

    update

--- Vue/vue-test.js
+++ Vue/vue-test.js
@@ -8,8 +8,10 @@ var app = new Vue({
 		onSale: "On Sale!",
 		inventory: 15,
 		details: ["80% silk", "20% polyester", "Ladies"],
+		mainPrice:0,
 		price: 999,
 		priceDown: 999 * 0.7,
+		total:0,
 		variants: [{
 				variantId: 2345,
 				variantSider: "Front",
@@ -29,12 +31,14 @@ var app = new Vue({
 			if (this.inventory > 0) {
 				this.cart += 1;
 				this.inventory -= 1;
+			this.total=this.cart*this.price;				
 			}
 		},
 		decrementToCart() {
 			if (this.cart > 0) {
 				this.cart -= 1;
 				this.inventory += 1;
+			this.total=this.cart*this.price;				
 			}
 		},
 		updateProduct(variantImage) {
