9163b1ce5b131ae90c0cfd236fd18144152f2cfb
Lê Thanh Tâm <37645432+thanhtamhp2018@users.noreply.github.com>
2018-05-22 17:10:30 +0700

    update

--- Vue/vue-test.js
+++ Vue/vue-test.js
@@ -10,7 +10,7 @@ var app = new Vue({
 		details: ["80% silk", "20% polyester", "Ladies"],
 		mainPrice:0,
 		price: 999,
-		priceDown: 999 * 0.7,
+		priceDown: Math.floor(999 * 0.7),
 		total:0,
 		variants: [{
 				variantId: 2345,
@@ -26,21 +26,34 @@ var app = new Vue({
 		sizes: ['S', 'M', 'L', 'XL', 'XXL', '3XL'],
 		cart: 0
 	},
+	//sau khi số lượng trong kho còn từ 10 cái trở xuống thì sẽ có giảm giá 30%, giá của priceDown,
+	//bắt đầu từ cái hàng đó sẽ được hưởng giá giảm, còn phía trước nó sẽ không được giảm, vẫn được
+	//tính với giá của price, và cart sẽ vẫn hiển thị số lượng
 	methods: {
 		addToCart() {
-			if (this.inventory > 0) {
-				this.cart += 1;
-				this.inventory -= 1;
-			this.total=this.cart*this.price;				
+            if (this.inventory > 0) {
+                this.cart += 1;
+                this.inventory -= 1;
+                let priceNow;
+                if (this.inventory>10) priceNow = this.price;
+                else priceNow = this.priceDown; 
+                console.log(priceNow);
+            this.total+=priceNow;                
 			}
-		},
-		decrementToCart() {
-			if (this.cart > 0) {
-				this.cart -= 1;
-				this.inventory += 1;
-			this.total=this.cart*this.price;				
+        },
+        decrementToCart() {
+            if (this.cart > 0) {
+                this.cart -= 1;
+                let priceNow;
+                if (this.inventory>10) priceNow = this.price;
+                else priceNow = this.priceDown; 
+                this.inventory += 1;
+                
+                
+                console.log (priceNow);
+                this.total-= priceNow;                
 			}
-		},
+        },
 		updateProduct(variantImage) {
 			this.image = variantImage;
 		}
